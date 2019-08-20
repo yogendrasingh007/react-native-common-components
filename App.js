7c6f987d0febf582441bc6714b23ff702ca8215b
yogendra <yogisingh4yu@gmail.com>
2019-08-20 18:57:59 +0530

    made working directory

--- App.js
+++ App.js
@@ -14,12 +14,11 @@ const instructions = Platform.select({
 	android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
+				<Text style={styles.welcome}>Welcome to React Native Component!</Text>
 				<Text style={styles.instructions}>To ge App.js</Text>
 				<Text style={styles.instructions}>{instructions}</Text>
 			</View>
