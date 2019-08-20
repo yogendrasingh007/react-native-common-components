b4b0778decdaa59e1ffa45ba8807ff5180b088ae
yogendra <yogisingh4yu@gmail.com>
2019-08-20 19:02:18 +0530

    made working directory

--- App.js
+++ App.js
@@ -14,12 +14,10 @@ const instructions = Platform.select({
 	android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-
 				<Text style={styles.welcome}>Welcome to</Text>
 				<Text style={styles.instructions}>To get started, edit App.js</Text>
 
